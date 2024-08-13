4ad7c07f2e68384ac5014066079927d8d0341f85
Shreshtth <shreshtthpersonal@gmail.com>
2024-08-13 16:35:41 +0530

    Updated test script.

--- src/test_3rd_phase.sh
+++ src/test_3rd_phase.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 python test.py \
-  --working_dir '../' \
+  --working-dir '../' \
+  --dataset-dir 'E:\Work\DatasetHandler\data' \
   --saved_fn 'ttnet_3rd_phase' \
   --gpu_idx 0 \
   --batch_size 1 \
   --pretrained_path ../checkpoints/ttnet_3rd_phase/ttnet_3rd_phase_epoch_30.pth \
   --seg_thresh 0.5 \
   --event_thresh 0.5 \
-  --smooth-labelling
+  --smooth-labelling \
+  --save_test_output
